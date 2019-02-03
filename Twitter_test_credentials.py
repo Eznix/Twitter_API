5f6914a227cf2d5960155e29d7fdd07924704229
Eznix <45634360+Eznix@users.noreply.github.com>
2019-02-03 18:01:12 +0100

    Nope

--- Twitter_test_credentials.py
+++ Twitter_test_credentials.py
@@ -1,4 +1,4 @@
-access_token = "612463228-1Li1bzvtq0FjoufVWFYjTMirnkp9nABF0lMRIcdt"
-access_token_secret = "X5TONT6jgItd5demR4G8kEuCwR6GCkMuNpMp4egZ79gID"
-consumer_key = "kBXuGQ1JQZb2iZ4cJb3K0TPLP"
-consumer_secret = "HM7P7kWSzOirXJ9gRi5hnDb9KSKAc4c1ib3gryaIn4jaMWB6TI"
+access_token = "Nope"
+access_token_secret = "Nope"
+consumer_key = "Nope"
+consumer_secret = "Nope"

7ad08d57e738563f28ad79849acaf23b66dbb1fd
root <root@localhost.localdomain>
2019-02-16 13:18:01 -0600

    initial commit

--- Twitter test.py
+++ Twitter test.py
@@ -3,7 +3,12 @@ from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+
 import Twitter_test_credentials
+import numpy as np
+import pandas as pd
+
+
 
 # # # # Twitter Client # # # #
 
@@ -15,6 +20,10 @@ class TwitterClient():
 
         self.twitter_user = twitter_user
 
+    def get_twitter_client_api(self):
+        return self.twitter_client
+    
+    
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
         for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
@@ -86,13 +95,39 @@ class TwitterListener(StreamListener):
             return False
         print(status)
 
+class TweetAnalyzer():
+    """
+    Funtionality for analyzing and categorizing content from tweets.
+    """
+
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns =['Tweets'])
+        
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        
+        return df
 
 if __name__ == "__main__":
-    hash_tag_tweets = ["Donald Trump"]
-    fetched_tweets_filename = "tweets.txt"
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+    
+    api = twitter_client.get_twitter_client_api()
 
-    twitter_client = TwitterClient('pycon')
-    print(twitter_client.get_user_timeline_tweets(50))
+    
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=20)
+    
+    #print(dir(tweets[0]))
+    #print(tweets[0].id)
+    
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
 
-    #twitter_streamer = TwitterStreamer()
-    #twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_tweets)
+    print(df.head(10))
+
+
+    
+
---
+++ Twitter_test_credentials.py
@@ -0,0 +1,4 @@
+access_token = "612463228-XNMqYHXlCPx0iymloRiKsk6RG48hNilso4l8ZTgA"
+access_token_secret = "LhbxsyQpODccrBFCXnX9v9TxTUO2zORb1RptsQWU7Pk0g"
+consumer_key = "YbZdXWrjVOGfHv36PtppMe5Ic"
+consumer_secret = "z71wKz2l0NlQTNZakZKUKEHQ23ekM8yLmyxArQlHbJbmfeFJBb"

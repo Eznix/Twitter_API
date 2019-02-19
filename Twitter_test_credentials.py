471f14dc9ee8abfd5c3da13200480594f84e02a6
Eznix <45634360+Eznix@users.noreply.github.com>
2019-02-19 18:41:29 +0100

    Delete Twitter_test_credentials.py

--- Twitter_test_credentials.py
+++
@@ -1,5 +0,0 @@
-access_token = "612463228-XNMqYHXlCPx0iymloRiKsk6RG48hNilso4l8ZTgA"
-access_token_secret = "LhbxsyQpODccrBFCXnX9v9TxTUO2zORb1RptsQWU7Pk0g"
-
-consumer_key = "YbZdXWrjVOGfHv36PtppMe5Ic"
-consumer_secret = "z71wKz2l0NlQTNZakZKUKEHQ23ekM8yLmyxArQlHbJbmfeFJBb"

96ad8287a0ff640a8252a02cb58a879e09f1e18a
root <root@localhost.localdomain>
2019-02-19 11:38:52 -0600

    Changes made

--- Twitter test.py
+++ Twitter test.py
@@ -9,6 +9,7 @@ from textblob import TextBlob
 import Twitter_test_credentials
 import numpy as np
 import pandas as pd
+import re 
 import matplotlib.pyplot as plt
 
 
@@ -35,7 +36,7 @@ class TwitterClient():
 
     def get_friend_list(self, num_friends):
         friend_list = []
-        for friend in Cursor(self.twitter_client.friend,id=self.twitter_user).items(num_friends):
+        for friend in Cursor(self.twitter_client.friends,id=self.twitter_user).items(num_friends):
             friend_list.append(friend)
         return friend_list
 
@@ -43,7 +44,7 @@ class TwitterClient():
         home_timeline_tweets = []
         for tweet in Cursor(self.twitter_client.home_timeline,id=self.twitter_user).items(num_tweets):
             home_timeline_tweets.append(tweet)
-        return get_home_timeline_tweets
+        return home_timeline_tweets
 
 # # # # Twitter Authenticator # # # #
 
@@ -55,6 +56,7 @@ class TwitterAuthenticator():
         auth.set_access_token(Twitter_test_credentials.access_token, Twitter_test_credentials.access_token_secret)
         return auth
 
+# # # # TWITTER STREAMER # # # #
 
 class TwitterStreamer():
     """
@@ -64,15 +66,17 @@ class TwitterStreamer():
     def __init__(self):
         self.twitter_autenticator = TwitterAuthenticator()
 
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_tweets):
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         # This handles Twitter authentication and the connection to the Twitter Streaming API.
         listener = TwitterListener(fetched_tweets_filename)
         auth = self.twitter_autenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
 
         # This filters Twitter Streams to capture data by the keywords:
-        stream.filter(hash_tag_tweets)
+        stream.filter(track=hash_tag_list)
 
+
+# # # # TWITTER STREAM LISTENER # # # #
 
 class TwitterListener(StreamListener):
     """
@@ -94,7 +98,7 @@ class TwitterListener(StreamListener):
 
     def on_error(self, status):
         if status == 420:
-            # Returning False on_data method in case rate limit is occurs to not get kicked.
+            # Returning False on_data method in case rate limit occurs to not get kicked.
             return False
         print(status)
 
@@ -102,6 +106,19 @@ class TweetAnalyzer():
     """
     Funtionality for analyzing and categorizing content from tweets.
     """
+
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(w+:\/\/\S+)", " ", tweet).split())
+        
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
 
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns =['tweets'])
@@ -112,36 +129,20 @@ class TweetAnalyzer():
         df['source'] = np.array([tweet.source for tweet in tweets])
         df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
         df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
-        
+        df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in tweets])
+       
         return df
 
 if __name__ == "__main__":
+
     twitter_client = TwitterClient()
     tweet_analyzer = TweetAnalyzer()
     
     api = twitter_client.get_twitter_client_api()
 
     tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)    
-    
+
     df = tweet_analyzer.tweets_to_data_frame(tweets)
-    
-    # Get average length over all tweets.
-    print(np.mean(df['len']))
 
-    # Get the number of likes for the most liked tweet.
-    print(np.max(df['likes']))
-    
-    # Get the number of retweets for the most retweeted tweet.
-    print(np.max(df['retweets']))
-            
-    #Time series
-    #time_likes = pd.Series(data=df['likes'].values, index = df['date'])
-    #time_likes.plot(figsize=(16,4), color='r')
-    #plt.show()
-
-    time_retweets = pd.Series(data=df['likes'].values, index = df['date'])
-    time_retweets.plot(figsize=(16,4), label="likes",legend=True)
-
-    time_retweets = pd.Series(data=df['retweets'].values, index = df['date'])
-    time_retweets.plot(figsize=(16,4), label="retweets",legend=True)
     plt.show()
+    print(df.head(10))
